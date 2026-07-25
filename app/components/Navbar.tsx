b4108457811a35564243a6d21d9a9a02a9a31f45
baarbi3 <ayan.whiz@gmail.com>
2026-07-25 15:15:28 +0500

    props made functional and filled for home screen

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import React from 'react'
 
 type ButtonType = {
@@ -6,13 +7,25 @@ type ButtonType = {
 };
 
 interface PropsType {
-  navButtons: ButtonType[]; 
+  navButtons: ButtonType[];
+  title?: boolean;
 }
 
 
 const Navbar = (props: PropsType) => {
   return (
-    <div className="flex w-full p-8 dark:bg-gray-800 bg-gray-500"></div>
+    <div className="flex w-full p-3 dark:bg-gray-900 bg-gray-500">
+      {props.title? <>
+        <Link className="whitespace-nowrap" href={"/"}>Dev Collaboration Hub</Link>
+      </> : <></>}
+      <div className="flex justify-center w-full">
+        {props.navButtons.map((button) => (
+          <div key={button.text} className="mx-4 border-b-2 border-transparent hover:border-gray-50">
+            <Link href={button.url}>{button.text}</Link>
+          </div>
+        ))}
+      </div>
+    </div>
   )
 }
 
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,34 @@
 import Image from "next/image";
+import Navbar from "./components/Navbar";
 
 export default function Home() {
+  const data = {
+    navButtons: [
+      {
+        text: "Home",
+        url: "#"
+      },
+      {
+        text: "Projects",
+        url: "#"
+      },
+      {
+        text: "Contributers",
+        url: "#"
+      },
+      {
+        text: "Contributers Guide",
+        url: "#"
+      },
+      {
+        text: "Contact",
+        url: "#"
+      }
+    ]
+  }
   return (
     <div className="dark:bg-black w-full">
+      <Navbar navButtons={data.navButtons} title></Navbar>
     </div>
   );
 }
